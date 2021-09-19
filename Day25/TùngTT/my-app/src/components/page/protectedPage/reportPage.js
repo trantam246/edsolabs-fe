8c416d4f0dd8925f5c798c33306bb81da516ef5c
TungTT <truongthanhtung2701.dev@gmail.com>
2021-09-19 23:34:57 +0700

    Update Date Range

--- Day25/TùngTT/my-app/src/components/page/protectedPage/reportPage.js
+++ Day25/TùngTT/my-app/src/components/page/protectedPage/reportPage.js
@@ -7,7 +7,19 @@ import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
-
+import DateFnsUtils from '@date-io/date-fns';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import { DateRange } from 'react-date-range';
+import 'react-date-range/dist/styles.css'; 
+import 'react-date-range/dist/theme/default.css';
+import {
+  DatePicker,
+  TimePicker,
+  DateTimePicker,
+  MuiPickersUtilsProvider,
+} from '@material-ui/pickers';
 import { Chart } from './barChart';
 const useStyles = makeStyles((theme) => ({
   modGrid: {
@@ -16,12 +28,31 @@ const useStyles = makeStyles((theme) => ({
   },
   btnPos: {
     textAlign: 'end'
-  }
+  },
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
 }));
 
 export const ReportPage = () => {
   const classes = useStyles();
   const [data, setData] = useState([]);
+  const [state, setState] = useState([
+    {
+      startDate: new Date(),
+      endDate: null,
+      key: 'selection'
+    }
+  ]);
+  
   useEffect(() => {
     retrieveTutorials();
   }, );
@@ -36,22 +67,40 @@ export const ReportPage = () => {
       });
   };
   // Menu
-  const [anchorEl, setAnchorEl] = React.useState(null);
-
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [showPicker, setPicker] = useState(null);
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
-
+  const handelClickDateRange = e => {
+    setPicker(e.currentTarget);
+  }
+  const handleCloseDateRange = () => {
+    setPicker(null)
+  }
   const handleClose = () => {
     setAnchorEl(null);
   };
   //-------------
+  //Open Date Range
+  const [open, setOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setOpen(false);
+  };
+
   return (
     <React.Fragment>
        <Grid container spacing={4}>
         <Grid item xs={6} className={classes.modGrid}>
           <Typography variant="h4">
-              This week:
+              <Button>
+                Click me
+              </Button>
           </Typography>
         </Grid>
         <Grid item xs={6}  className={classes.modGrid} className={classes.btnPos}>
@@ -72,14 +121,40 @@ export const ReportPage = () => {
             open={Boolean(anchorEl)}
             onClose={handleClose}
           >
-          <MenuItem onClick={handleClose} label="Today">
-          </MenuItem>
+          <MenuItem onClick={handleClose}>Today</MenuItem>
           <MenuItem onClick={handleClose}>Yesterday</MenuItem>
           <MenuItem onClick={handleClose}>This week</MenuItem>
           <MenuItem onClick={handleClose}>Last week</MenuItem>
           <MenuItem onClick={handleClose}>This month</MenuItem>
           <MenuItem onClick={handleClose}>Last month</MenuItem>
-          <MenuItem onClick={handleClose}>Date range</MenuItem>
+          <MenuItem onClick={handleOpenModal}>
+            Date Range
+          </MenuItem>
+          <Modal
+            aria-labelledby="transition-modal-title"
+            aria-describedby="transition-modal-description"
+            className={classes.modal}
+            open={open}
+            onClose={handleCloseModal}
+            closeAfterTransition
+            BackdropComponent={Backdrop}
+            BackdropProps={{
+              timeout: 500,
+            }}>
+            <Fade in={open}>
+              <div className={classes.paper}>
+                <DateRange
+                  editableDateInputs={true}
+                  onChange={item => {
+                    setState([item.selection])
+                    console.log(state)
+                  }}
+                  moveRangeOnFirstSelection={false}
+                  ranges={state}
+                  />
+              </div>
+            </Fade>
+          </Modal>
           </Menu>
         </Grid>
       </Grid>
