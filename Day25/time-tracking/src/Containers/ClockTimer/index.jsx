87b51f1bbb9118d9e89a3326ad7233b13ad4ec1d
thanhhoang <maketwithxunu@gmail.com>
2021-09-20 18:02:12 +0700

    fix clock time from poor to pro ^^

--- Day25/time-tracking/src/Containers/ClockTimer/index.jsx
+++ Day25/time-tracking/src/Containers/ClockTimer/index.jsx
@@ -1,45 +1,37 @@
 import React, { useState, useEffect, useRef } from 'react';
 
 export const ClockTimer = ({ onHandleTimeSpend = () => {} }) => {
-  const [clockTime, setClockTime] = useState({ hour: 0, minute: 0, second: 0 });
+  const [clockTime, setClockTime] = useState({});
   const clockTimeRef = useRef({ hour: 0, minute: 0, second: 0 });
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setClockTime({
-        ...clockTime,
-        second:
-          clockTime.second === 59
-            ? ((clockTime.second = 0), (clockTime.minute += 1))
-            : (clockTime.second += 1),
-        hour: clockTime.minute === 59 ? (clockTime.hour += 1) : clockTime.hour,
-      });
-
-      if (clockTimeRef.current.second === 59) {
+      if (clockTimeRef.current.second === 60) {
         clockTimeRef.current.second = 0;
         clockTimeRef.current.minute++;
       } else {
         clockTimeRef.current.second++;
       }
-      if (clockTimeRef.current.minutes === 59) {
+      if (clockTimeRef.current.minutes === 60) {
         clockTimeRef.hour++;
       }
+
+      setClockTime({});
     }, 1000);
     return () => {
       clearInterval(interval);
-      clockTimeRef.current.second = 0;
-      clockTimeRef.current.minute = 0;
-      clockTimeRef.current.hour = 0;
     };
   }, []);
 
   useEffect(() => {
-    if (clockTime.hour >= 1) {
-      onHandleTimeSpend(clockTime.hour * 60 + clockTime.minute);
+    if (clockTimeRef.current.hour >= 1) {
+      onHandleTimeSpend(
+        clockTimeRef.current.hour * 60 + clockTimeRef.current.minute,
+      );
     } else {
-      onHandleTimeSpend(clockTime.minute);
+      onHandleTimeSpend(clockTimeRef.current.minute);
     }
-  }, [clockTime.minute]);
+  }, [clockTimeRef.current.minute]);
 
   return (
     <div>
